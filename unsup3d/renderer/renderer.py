0029a475b48eb5d15256b9fd201dd2bc00448bf1
hoaithi <phunghoaithi137@gmail.com>
2024-05-16 17:51:09 +0700

    refactor: renderer

--- unsup3d/renderer/renderer.py
+++ unsup3d/renderer/renderer.py
@@ -1,11 +1,29 @@
 import torch
 import math
-import neural_renderer as nr
 from .utils import *
 
+from pytorch3d.utils import ico_sphere
+from pytorch3d.io import load_obj
+from pytorch3d.structures import Meshes
+from pytorch3d.ops import sample_points_from_meshes
+from pytorch3d.loss import chamfer_distance
+import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+from pytorch3d.renderer import (
+    look_at_view_transform,
+    FoVPerspectiveCameras,
+    PointLights,
+    DirectionalLights,
+    Materials,
+    RasterizationSettings,
+    MeshRenderer,
+    MeshRasterizer,
+    SoftPhongShader,
+    TexturesUV,
+    TexturesVertex
+)
 
 EPS = 1e-7
-
 
 class Renderer():
     def __init__(self, cfgs):
@@ -25,29 +43,42 @@ class Renderer():
         #             (1)   (z)
 
         ## renderer for visualization
-        R = [[[1.,0.,0.],
-              [0.,1.,0.],
-              [0.,0.,1.]]]
+        R = [[[1., 0., 0.],
+              [0., 1., 0.],
+              [0., 0., 1.]]]
         R = torch.FloatTensor(R).to(self.device)
-        t = torch.zeros(1,3, dtype=torch.float32).to(self.device)
-        fx = (self.image_size-1)/2/(math.tan(self.fov/2 *math.pi/180))
-        fy = (self.image_size-1)/2/(math.tan(self.fov/2 *math.pi/180))
-        cx = (self.image_size-1)/2
-        cy = (self.image_size-1)/2
+        t = torch.zeros(1, 3, dtype=torch.float32).to(self.device)
+        fx = (self.image_size - 1) / 2 / (math.tan(self.fov / 2 * math.pi / 180))
+        fy = (self.image_size - 1) / 2 / (math.tan(self.fov / 2 * math.pi / 180))
+        cx = (self.image_size - 1) / 2
+        cy = (self.image_size - 1) / 2
         K = [[fx, 0., cx],
              [0., fy, cy],
              [0., 0., 1.]]
         K = torch.FloatTensor(K).to(self.device)
         self.inv_K = torch.inverse(K).unsqueeze(0)
         self.K = K.unsqueeze(0)
-        self.renderer = nr.Renderer(camera_mode='projection',
-                                    light_intensity_ambient=1.0,
-                                    light_intensity_directional=0.,
-                                    K=self.K, R=R, t=t,
-                                    near=self.renderer_min_depth, far=self.renderer_max_depth,
-                                    image_size=self.image_size, orig_size=self.image_size,
-                                    fill_back=True,
-                                    background_color=[1,1,1])
+
+        lights = PointLights(device=self.device, location=[[0.0, 0.0, 0.0]])
+        cameras = FoVPerspectiveCameras(device=self.device, R=R, T=t, fov=self.fov)
+
+        self.rasterizer = MeshRasterizer(
+            cameras=cameras,
+            raster_settings=RasterizationSettings(
+                image_size=64,
+                blur_radius=0.0,
+                faces_per_pixel=1,
+            )
+        )
+
+        self.renderer = MeshRenderer(
+            rasterizer=self.rasterizer,
+            shader=SoftPhongShader(
+                device=self.device,
+                cameras=cameras,
+                lights=lights
+            )
+        )
 
     def set_transform_matrices(self, view):
         self.rot_mat, self.trans_xyz = get_transform_matrices(view)
@@ -64,7 +95,7 @@ class Renderer():
 
     def depth_to_3d_grid(self, depth):
         b, h, w = depth.shape
-        grid_2d = get_grid(b, h, w, normalize=False).to(depth.device)  # Nxhxwx2
+        grid_2d = get_grid(b, h, w, normalize=False).to(depth.device)  # N x h x w x 2
         depth = depth.unsqueeze(-1)
         grid_3d = torch.cat((grid_2d, torch.ones_like(depth)), dim=3)
         grid_3d = grid_3d.matmul(self.inv_K.to(depth.device).transpose(2,1)) * depth
@@ -80,10 +111,10 @@ class Renderer():
 
     def get_warped_3d_grid(self, depth):
         b, h, w = depth.shape
-        grid_3d = self.depth_to_3d_grid(depth).reshape(b,-1,3)
+        grid_3d = self.depth_to_3d_grid(depth).reshape(b, -1, 3)
         grid_3d = self.rotate_pts(grid_3d, self.rot_mat)
         grid_3d = self.translate_pts(grid_3d, self.trans_xyz)
-        return grid_3d.reshape(b,h,w,3) # return 3d vertices
+        return grid_3d.reshape(b, h, w, 3) # return 3d vertices
 
     def get_inv_warped_3d_grid(self, depth):
         b, h, w = depth.shape
@@ -106,14 +137,20 @@ class Renderer():
 
     def warp_canon_depth(self, canon_depth):
         b, h, w = canon_depth.shape
-        grid_3d = self.get_warped_3d_grid(canon_depth).reshape(b,-1,3)
+        grid_3d = self.get_warped_3d_grid(canon_depth).reshape(b, -1, 3) # b x (hxw) x 3
         faces = get_face_idx(b, h, w).to(canon_depth.device)
-        warped_depth = self.renderer.render_depth(grid_3d, faces)
+
+        meshes = Meshes(verts=grid_3d, faces=faces)
+        warped_depth = self.rasterizer(meshes).zbuf.squeeze(3)
+
+        ############################################################
+        # warped_depth = self.renderer.render_depth(grid_3d, faces)
+        ############################################################
 
         # allow some margin out of valid range
-        margin = (self.max_depth - self.min_depth) /2
+        margin = (self.max_depth - self.min_depth) / 2
         warped_depth = warped_depth.clamp(min=self.min_depth-margin, max=self.max_depth+margin)
-        return warped_depth
+        return warped_depth.flip(1).flip(2)
 
     def get_normal_from_depth(self, depth):
         b, h, w = depth.shape
@@ -129,8 +166,9 @@ class Renderer():
         normal = normal / (((normal**2).sum(3, keepdim=True))**0.5 + EPS)
         return normal
 
-    def render_yaw(self, im, depth, v_before=None, v_after=None, rotations=None, maxr=90, nsample=9, crop_mesh=None):
-        b, c, h, w = im.shape
+
+    def render_yaw(self, imgs, depth, v_before=None, v_after=None, rotations=None, maxr=90, nsample=9, crop_mesh=None):
+        b, c, h, w = imgs.shape
         grid_3d = self.depth_to_3d_grid(depth)
 
         if crop_mesh is not None:
@@ -148,21 +186,21 @@ class Renderer():
                 grid_3d[:,:,-right:,0] = grid_3d[:,:,-right-1:-right,0].repeat(1,1,right)
                 grid_3d[:,:,-right:,2] = grid_3d[:,:,-right-1:-right,2].repeat(1,1,right)
 
-        grid_3d = grid_3d.reshape(b,-1,3)
+        grid_3d = grid_3d.reshape(b, -1, 3)
         im_trans = []
 
         # inverse warp
         if v_before is not None:
             rot_mat, trans_xyz = get_transform_matrices(v_before)
             grid_3d = self.translate_pts(grid_3d, -trans_xyz)
-            grid_3d = self.rotate_pts(grid_3d, rot_mat.transpose(2,1))
+            grid_3d = self.rotate_pts(grid_3d, rot_mat.transpose(2, 1))
 
         if rotations is None:
-            rotations = torch.linspace(-math.pi/180*maxr, math.pi/180*maxr, nsample)
+            rotations = torch.linspace(-math.pi / 180 * maxr, math.pi / 180 * maxr, nsample)
         for i, ri in enumerate(rotations):
-            ri = torch.FloatTensor([0, ri, 0]).to(im.device).view(1,3)
+            ri = torch.FloatTensor([0, ri, 0]).to(imgs.device).view(1, 3)
             rot_mat_i, _ = get_transform_matrices(ri)
-            grid_3d_i = self.rotate_pts(grid_3d, rot_mat_i.repeat(b,1,1))
+            grid_3d_i = self.rotate_pts(grid_3d, rot_mat_i.repeat(b, 1, 1))
 
             if v_after is not None:
                 if len(v_after.shape) == 3:
@@ -173,8 +211,45 @@ class Renderer():
                 grid_3d_i = self.rotate_pts(grid_3d_i, rot_mat)
                 grid_3d_i = self.translate_pts(grid_3d_i, trans_xyz)
 
-            faces = get_face_idx(b, h, w).to(im.device)
-            textures = get_textures_from_im(im, tx_size=self.tex_cube_size)
-            warped_images = self.renderer.render_rgb(grid_3d_i, faces, textures).clamp(min=-1., max=1.)
-            im_trans += [warped_images]
+            faces = get_face_idx(b, h, w).to(imgs.device)
+            textures = imgs.permute(0, 2, 3, 1).reshape(b, -1, 3)
+
+            meshes = Meshes(verts=grid_3d_i, faces=faces)
+            meshes.textures = TexturesVertex(verts_features=textures)
+
+            warped_images = self.renderer(meshes).clamp(min=-1., max=1.)
+            warped_images = warped_images[:, :, :, :3].permute(0, 3, 1, 2)
+
+
+            ###############################################################
+            # warped_images = self.renderer.render_rgb(grid_3d_i, faces, textures).clamp(min=-1., max=1.)
+            ###############################################################
+            im_trans += [warped_images.flip(2)]
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            save_images(current_dir, warped_images, 'warped', str(i), sep_folder=True)
         return torch.stack(im_trans, 1)  # b x t x c x h x w
+
+
+import numpy as np
+import cv2
+import os 
+
+
+def mkdir(path):
+    """Create directory PATH recursively if it does not exist."""
+    os.makedirs(path, exist_ok=True)
+
+def save_images(out_fold, imgs, prefix='', suffix='', sep_folder=True, ext='.png'):
+    imgs = imgs.transpose(2,3,1)
+    if sep_folder:
+        out_fold = os.path.join(out_fold, suffix)
+    mkdir(out_fold)
+    prefix = prefix + '_' if prefix else ''
+    suffix = '_' + suffix if suffix else ''
+    offset = len(glob.glob(os.path.join(out_fold, prefix+'*'+suffix+ext))) +1
+    for i, img in enumerate(imgs):
+        if 'depth' in suffix:
+            im_out = np.uint16(img[...,::-1]*65535.)
+        else:
+            im_out = np.uint8(img[...,::-1]*255.)
+        cv2.imwrite(os.path.join(out_fold, prefix+'%05d'%(i+offset)+suffix+ext), im_out)
