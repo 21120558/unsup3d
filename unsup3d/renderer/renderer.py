5292ed4995c8c805f13c902a9f1ccd463377930d
hoaithi <phunghoaithi137@gmail.com>
2024-05-21 21:52:34 +0700

    add: data

--- unsup3d/renderer/renderer.py
+++ unsup3d/renderer/renderer.py
@@ -51,7 +51,7 @@ class Renderer():
         #       [0., 0., 1.]]]
         # R = torch.FloatTensor(R).to(self.device)
         # t = torch.zeros(1, 3, dtype=torch.float32).to(self.device)
-        R, t = look_at_view_transform(1, device=self.device)
+        R, t = look_at_view_transform(dist=2, device=self.device)
         fx = (self.image_size - 1) / 2 / (math.tan(self.fov / 2 * math.pi / 180))
         fy = (self.image_size - 1) / 2 / (math.tan(self.fov / 2 * math.pi / 180))
         cx = (self.image_size - 1) / 2
@@ -172,8 +172,6 @@ class Renderer():
         # allow some margin out of valid range
         margin = (self.max_depth - self.min_depth) / 2
         warped_depth = warped_depth.clamp(min=self.min_depth - margin, max=self.max_depth + margin)
-        warped_depth[warped_depth == self.min_depth - margin] = self.max_depth
-
 
         return warped_depth.flip(1).flip(2)
 
