f9a481ac7ed50f268e336cb3a97ce7cf5d4aea06
hoaithi <phunghoaithi137@gmail.com>
2024-05-22 18:42:20 +0700

    update

--- unsup3d/renderer/renderer.py
+++ unsup3d/renderer/renderer.py
@@ -51,7 +51,7 @@ class Renderer():
         #       [0., 0., 1.]]]
         # R = torch.FloatTensor(R).to(self.device)
         # t = torch.zeros(1, 3, dtype=torch.float32).to(self.device)
-        R, t = look_at_view_transform(dist=-0.001, elev=0.75, at=((0, -0.015, 0),), device=self.device)
+        R, t = look_at_view_transform(dist=-0.001, elev=1.125, at=((0, -0.0225, 0),), device=self.device)
         fx = (self.image_size - 1) / 2 / (math.tan(self.fov / 2 * math.pi / 180))
         fy = (self.image_size - 1) / 2 / (math.tan(self.fov / 2 * math.pi / 180))
         cx = (self.image_size - 1) / 2
